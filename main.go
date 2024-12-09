8912789cc49a1a300e57d8fc3d504fab6215a540
MauroRaya <maurorayabr@gmail.com>
2024-12-09 20:33:40 -0300

    feat: add form validation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -11,19 +12,39 @@ type Item struct {
 	Quantity int
 }
 
+func newItem(name string, quantity int) Item {
+	return Item{
+		Name:     name,
+		Quantity: quantity,
+	}
+}
+
+type Error struct {
+	IsNameInvalid     bool
+	IsQuantityInvalid bool
+}
+
+func newError(isNameInvalid, isQuantityInvalid bool) Error {
+	return Error{
+		IsNameInvalid:     isNameInvalid,
+		IsQuantityInvalid: isQuantityInvalid,
+	}
+}
+
 func main() {
 	r := gin.Default()
 
 	items := []Item{
-		{Name: "banana", Quantity: 50},
-		{Name: "cake", Quantity: 12},
-		{Name: "orange", Quantity: 20},
+		newItem("banana", 50),
+		newItem("cake", 10),
+		newItem("orange", 20),
 	}
 
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/items", func(c *gin.Context) {
 		c.HTML(200, "index", gin.H{
+			"Error": Error{},
 			"Items": items,
 		})
 	})
@@ -32,13 +53,36 @@ func main() {
 		name := c.PostForm("Name")
 		quantity := c.PostForm("Quantity")
 
-		quantityInt, _ := strconv.Atoi(quantity)
+		//validação nome duplicado
+		for i := range items {
+			if items[i].Name == name {
+				c.HTML(422, "form", gin.H{
+					"Error": newError(true, false),
+				})
+				return
+			}
+		}
+
+		//validação quantidade
+		quantityInt, err := strconv.Atoi(quantity)
+
+		if err != nil {
+			c.HTML(422, "form", gin.H{
+				"Error": newError(false, true),
+			})
+			return
+		}
 
 		newItem := Item{name, quantityInt}
-
 		items = append(items, newItem)
 
-		c.HTML(200, "items", gin.H{
+		c.HTML(200, "form", gin.H{
+			"Error": Error{},
+		})
+
+		fmt.Println(items)
+
+		c.HTML(200, "oob-items", gin.H{
 			"Items": items,
 		})
 	})
